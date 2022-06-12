3e67ad22a0830142ebd46b36b12f3a6f2bc62c14
Maia Cochran <maia.cochran1@gmail.com>
2022-06-12 01:45:12 -0700

    Remove extra psuedocode & comments

--- player.js
+++ player.js
@@ -16,15 +16,3 @@ class Player {
     this.wins++;
   }
 }
-
-//create an array, housing the buddy options
-//need a randomizer method to generate the computer's response
-//should have a method for each game choice
-//update wins and keep score
-
-
-//if the returned class name matches the returned class,
-//then it is a tie, if it equals either of the other ones,
-//one person will win
-//when the person wins, the score will increase and push
-//to the winning score and update it to reflect +1
