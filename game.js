9f6acf8b91807cd36f03684d6fd0b2c5028115d1
Maia Cochran <maia.cochran1@gmail.com>
2022-06-12 08:54:11 -0700

    Add psuedocode for game class

--- game.js
+++ game.js
@@ -3,6 +3,15 @@ class Game {
     this.id = Date.now();
     this.playerMe = playerMe;
     this.playerComputer = playerComputer;
-    
+
   }
+
 }
+
+
+//needs game choice, player choice
+//two instances of player
+//one instance to hold the user choice
+//one for computer choice
+//different for each round played
+//needs to determine the game type first
