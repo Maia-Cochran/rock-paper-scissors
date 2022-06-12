0dcd270ce4d9cceebb1b78cadabd165cb5ff5be3
Maia Cochran <maia.cochran1@gmail.com>
2022-06-12 12:22:26 -0700

    Add functionality to game class

--- game.js
+++ game.js
@@ -4,12 +4,43 @@ class Game {
     this.user = new Player ("user", 🤠 ) ;
     this.computer = new Player ("computer", 💻 );
     this.gameVersionChosen = 'classic' || 'spicy';
-    this.userBuddyChoices = ['rock', 'paper', 'scissors'];
-    this.computerBuddyChoices = ['rock', 'paper', 'scissors', 'alien', 'ufo'];
-    this.winner = true;
+    this.classicBuddyChoices = ['rock', 'paper', 'scissors'];
+    this.spicyBuddyChoices = ['rock', 'paper', 'scissors', 'alien', 'ufo'];
+    this.userWinner = true;
   }
-  checkForWinner(userChoice, gameVersion){
-
+  checkForWinner(userChoice, generatedChoice){
+    if(userChoice === generatedChoice){
+      return `Great minds think alike! It's a tie 🤪 `
+    } else if(userChoice === 'rock' && generatedChoice === 'paper' || generatedChoice === 'UFO'){
+      this.computer.winCount();
+      this.userWinner = false;
+    } else if(userChoice === 'rock' && generatedChoice === 'scissors' || generatedChoice === 'alien'){
+      this.user.winCount();
+      this.userWinner = true;
+    } else if(userChoice === 'paper' && generatedChoice === 'scissors' || generatedChoice === 'alien'){
+      this.computer.winCount();
+      this.userWinner = false;
+    } else if(userChoice === 'paper' && generatedChoice === 'rock' || generatedChoice === 'UFO'){
+      this.user.winCount();
+      this.userWinner = true;
+    } else if(userChoice === 'scissors' && generateChoice === 'paper' || generatedChoice === 'alien'){
+      this.user.winCount();
+      this.userWinner = true;
+    } else if(userChoice === 'scissors' && generatedChoice === 'rock' || generatedChoice === 'UFO'){
+      this.computer.winCount();
+      this.userWinner = false;
+    } else if(userChoice === 'alien' && generatedChoice === 'rock' || generatedChoice === 'scissors'){
+      this.computer.winCount();
+      this.userWinner = false;
+    } else if(userChoice === 'alien' && generatedChoice === 'paper' || generatedChoice === 'UFO'){
+      this.user.winCount();
+      this.userWinner = true;
+    } else if(userChoice === 'UFO' && generatedChoice === 'scissors' || generatedChoice === 'rock'){
+      this.user.winCount();
+      this.userWinner = true;
+    } else if(userChoice === 'UFO' && generatedChoice === 'alien' || generatedChoice === 'paper')
+    this.computer.winCount();
+    this.userWinner = false;
   }
 }
 
