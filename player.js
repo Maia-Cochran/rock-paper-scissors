f60a1e057cde451428c9901ad550ee0a90b13172
Maia Cochran <maia.cochran1@gmail.com>
2022-06-12 01:23:12 -0700

    Add player class & pseudocode functionality

---
+++ player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(win, score){
+    this.win = win;
+  }
+}
+
+
+
+//create an array, housing the buddy options
+//need a randomizer method to generate the computer's response
+//should have a method for each game choice
+//update wins and keep score
+
+
+//if the returned class name matches the returned class,
+//then it is a tie, if it equals either of the other ones,
+//one person will win
+//when the person wins, the score will increase and push
+//to the winning score and update it to reflect +1
