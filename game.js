ed732038d0a90589988269305899925c97c1f8ae
Maia Cochran <maia.cochran1@gmail.com>
2022-06-12 12:23:35 -0700

    Remove extra psuedocode

--- game.js
+++ game.js
@@ -43,14 +43,3 @@ class Game {
     this.userWinner = false;
   }
 }
-
-
-//needs game choice, player choice
-//two instances of player
-//one instance to hold the user choice
-//one for computer choice
-//different for each round played
-//needs to determine the game type first
-
-//should have variables for choices selected
-//will randomize the arrays given from player class
